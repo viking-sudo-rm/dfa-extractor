180c9c49fd15ab7f646b557ac9f8f8c4495dc4f5
Will Merrill <willm@allenai.org>
2021-12-07 15:45:19 -0500

    Reimplemented Tomita grammars to be sampled more efficiently.

--- languages.py
+++ languages.py
@@ -14,6 +14,38 @@ class Tomita2:
 
 class Tomita3:
 
+    """An odd number of a's must be followed be an even number of b's.
+    
+    Choose tokens randomly, except at the last index.
+    """
+
+    def generate(self, min_n, max_n):
+        for n in range(min_n, max_n):
+            yield self.sample(n)
+    
+    def sample(self, n):
+        """Running through a DFA."""
+        state = "even_a"
+        tokens = []
+        for i in range(n):
+            if state == "even_a":
+                token = random.choice(["a", "b"])
+                state = "odd_a" if token == "a" else "even_a"
+            elif state == "odd_a":
+                if i == n - 1:
+                    token = "a"
+                else:
+                    token = random.choice(["a", "b"])
+                state = "even_a" if token == "a" else "odd_b"
+            elif state == "odd_b":
+                token = "b"
+                state = "even_b"
+            else:  # even_b
+                token = random.choice(["a", "b"])
+                state = "odd_a" if token == "a" else "odd_b"
+            tokens.append(token)
+        return "".join(tokens)
+
     # ugly, replace with regex
     def valid(self, str):
         count_a, count_b = 0, 0
@@ -34,56 +66,60 @@ class Tomita3:
                 count_b += 1
         return not (count_b % 2 == 1 and count_a % 2 == 1)
 
-    def generate(self, min_n, max_n):
-        for n in range(min_n, max_n):
-            cand = ''.join(random.choices("ab", k=n))
-            while (not (self.valid(cand))): # one may use instead the := operator
-                cand = ''.join(random.choices("ab", k=n))
-            yield cand
+    # def generate(self, min_n, max_n):
+    #     for n in range(min_n, max_n):
+    #         cand = ''.join(random.choices("ab", k=n))
+    #         while (not (self.valid(cand))): # one may use instead the := operator
+    #             cand = ''.join(random.choices("ab", k=n))
+    #         yield cand
 
 class Tomita4:
 
-    # ugly, replace with regex
-    def valid(self, str):
-        count = 0
-        for c in str:
-            if (c == 'b'):
-                count += 1
-                if (count == 3):
-                    return False
-            else:
-                count = 0
-        return True
+    """All strings where three a's don't occur in a row."""
 
     def generate(self, min_n, max_n):
+        # Don't really need to generate one per length here, but we do.
         for n in range(min_n, max_n):
-            cand = ''.join(random.choices("ab", k=n))
-            while (not (self.valid(cand))): # one may use instead the := operator
-                cand = ''.join(random.choices("ab", k=n))
-            yield cand
+            state = 0
+            tokens = []
+            for _ in range(n):
+                token = "b" if state == 2 else random.choice(["a", "b"])                    
+                tokens.append(token)
+                if token == "b":
+                    state = 0
+                else:
+                    state += 1
+            yield "".join(tokens)
+
+
 
 class Tomita5:
 
-    # ugly, replace with regex
-    def valid(self, str):
-        count_a , count_b = 0, 0
-        for c in str:
-            if (c == 'b'):
-                count_b += 1
-            else:
-                count_a += 1
-        return (count_a % 2 == 0 and count_b % 2 == 0)
+    """All strings w where #_a(w) and #_b(w) are even."""
 
     def generate(self, min_n, max_n):
         for n in range(min_n, max_n):
-            if (n % 2 == 1):
-                continue
-            cand = ''.join(random.choices("ab", k=n))
-            while (not (self.valid(cand))): # one may use instead the := operator
-                cand = ''.join(random.choices("ab", k=n))
-            yield cand
+            if n % 2 == 0:
+                yield self.sample(n)
+                yield self.sample(n)
+        
+
+    def sample(self, n):
+        """Where n must be divisible by 2."""
+        k = n // 2
+        k1 = random.randint(0, k)
+        k2 = k - k1
+        tokens = []
+        tokens.extend("a" for _ in range(2 * k1))
+        tokens.extend("b" for _ in range(2 * (k2)))
+        random.shuffle(tokens)
+        return "".join(tokens)
 
 class Tomita6:
+
+    """The number of a's and b's is the same mod 3.
+    
+    Can do this the dumb way since it should be relatively fast."""
 
     # ugly, replace with regex
     def valid(self, str):
